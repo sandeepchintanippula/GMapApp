ae816bd879239448b9b9c91611f7c57760bb7aa8
student <student@achiever45.local>
2017-06-16 12:33:24 +0530

    Initial Commit

---
+++ GMapApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GMapApp
+//
+//  Created by student on 6/16/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
